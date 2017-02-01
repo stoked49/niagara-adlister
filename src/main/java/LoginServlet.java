26518d617e0e8521255ff56bd43a24b051ffaab7
Pierre Irby <stoked49@yahoo.com>
2017-02-01 11:16:57 -0600

    Completed MVC.  Beginning work on Sessions and Cookies.

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,33 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response) throws IOException
+    {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+
+        if (validAttempt) {
+            // This is how we know that a user has been authenticated
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
+
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
